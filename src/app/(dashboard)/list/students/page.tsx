f4c3915ed6bdfdbef2ac7112499e1b9f4f754554
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2025-04-03 17:46:09 +0800

    feat: add react-big-calendar

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -111,7 +111,7 @@ const StudentListPage = async (
       <td className="hidden lg:table-cell">{item.lecturer?.name || "-"}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/lecturers/${item.id}`}>
+          <Link href={`/list/stundents/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-ternary">
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
