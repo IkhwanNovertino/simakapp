af230526767e9caa7b2568a193cab68f1f39999e
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2025-04-22 11:10:48 +0800

    fix: fix update pagination

--- src/component/Pagination.tsx
+++ src/component/Pagination.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 const Pagination = ({ page, count }: { page: number, count: number }) => {
   const router = useRouter();
+  const totalPage = Math.ceil(count / ITEM_PER_PAGE);
   const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
   const hasNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
 
@@ -14,8 +15,28 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
     router.push(`${window.location.pathname}?${params}`);
   };
 
+  const getPageNumbers = () => {
+    const pages: (number | string)[] = [];
+
+    if (totalPage <= 5) {
+      for (let i = 1; i <= totalPage; i++) pages.push(i);
+    } else {
+      if (page <= 3) {
+        pages.push(1, 2, 3, "...", totalPage);
+      } else if (page >= totalPage - 2) {
+        pages.push(1, "...", totalPage - 2, totalPage - 1, totalPage);
+      } else {
+        pages.push(1, "...", page - 1, page, page + 1, "...", totalPage);
+      }
+    }
+
+    return pages;
+  };
+
+  console.log(getPageNumbers());
+
   return (
-    <div className="p-4 flex items-center justify-between text-gray-500">
+    <div className="p-4 flex flex-wrap items-center justify-center sm:justify-between text-gray-500">
       <button
         className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold hover:cursor-pointer hover:not-disabled:bg-primary disabled:opacity-50  disabled:cursor-not-allowed"
         onClick={() => { changePage(page - 1) }}
@@ -24,7 +45,22 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
         Prev
       </button>
       <div className="flex items-center gap-2 text-sm">
-        {Array.from(
+        {getPageNumbers().map((p, index) =>
+          typeof p === "string" ? (
+            <span key={index} className="px-2">
+              ...
+            </span>
+          ) : (
+            <button
+              key={index}
+              className={`px-2 rounded-sm hover:bg-primary cursor-pointer ${page === p ? "bg-primary-light" : ""}`}
+              onClick={() => { changePage(p) }}
+            >
+              {p}
+            </button>
+          )
+        )}
+        {/* {Array.from(
           { length: Math.ceil(count / ITEM_PER_PAGE) },
           (_, i) => {
             const pageIndex = i + 1;
@@ -38,7 +74,7 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
               </button>
             )
           }
-        )}
+        )} */}
       </div>
       <button
         className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold hover:cursor-pointer hover:bg-primary disabled:opacity-50  disabled:cursor-not-allowed"
